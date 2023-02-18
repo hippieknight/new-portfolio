5a657cf45af2df0ff9d904d3555b0939fa692cc6
NhatHiep-isme <55236520+NhatHiep-isme@users.noreply.github.com>
2023-02-18 18:31:59 +0700

    feat: add animation

--- public/components/introduction/Introduction.js
+++ public/components/introduction/Introduction.js
@@ -6,8 +6,12 @@ const Introduction = () => {
     <IntroductionWrapper>
       <Wrapper>
         <Avatar src="../images/avatar.jpg" />
-        <Name>- Nhat Hiep Tran -</Name>
-        <Position>Front-end Developer</Position>
+        <NameWrap>
+          <Name>- Nhat Hiep Tran -</Name>
+        </NameWrap>
+        <PositionWrap>
+          <Position>Front-end Developer</Position>
+        </PositionWrap>
         <SocialContact />
       </Wrapper>
     </IntroductionWrapper>
@@ -51,21 +55,79 @@ const Avatar = styled.img`
   }
 `;
 
-const Name = styled.h2`
+const NameWrap = styled.div`
+  width: fit-content;
   margin: 24px;
+`;
+const Name = styled.h2`
   font-size: 30px;
   font-weight: 300;
   font-family: Kaushan Script;
+  overflow: hidden;
   transform: rotate(-7deg);
   letter-spacing: 0.1em;
+  white-space: nowrap;
+  border-right: 2px solid white;
+
+  @keyframes type {
+    from {
+      width: 0;
+    }
+    99.9% {
+      border-right: 2px solid white;
+    }
+    to {
+      border-right: none;
+      width: 100%;
+    }
+  }
+  @keyframes blink-caret {
+    from {
+      border-color: transparent;
+    }
+    50% {
+      border-color: white;
+    }
+    to {
+      border-color: transparent;
+    }
+  }
+
+  animation: type 2s steps(18, jump-end) forwards;
 
   @media (min-width: 1024px) {
     font-size: 42px;
   }
 `;
+
+const PositionWrap = styled.div`
+  width: fit-content;
+`;
 const Position = styled.p`
+  opacity: 0;
   font-size: 14px;
   font-weight: 500;
+  overflow: hidden;
+  white-space: nowrap;
+  border-right: 2px solid white;
+
+  @keyframes type {
+    from {
+      width: 0;
+    }
+    1% {
+      opacity: 1;
+    }
+    99.9% {
+      border-right: 2px solid white;
+    }
+    to {
+      border-right: none;
+      width: 100%;
+      opacity: 1;
+    }
+  }
+  animation: type 2s steps(20, jump-end) 2s forwards;
   @media (min-width: 1024px) {
     font-size: 16px;
   }
